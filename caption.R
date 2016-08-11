a8880a5d155043e4689416599d074910ae5bafdb
MurakamiKeach <m.keach.11@gmail.com>
2016-08-11 22:37:20 +0900

    パス図をDrフォルダへ。使うかは微妙。 あとは論文中の図を微修正、本文をわずかに修正など。 3091 words

---
+++ caption.R
@@ -0,0 +1,11 @@
+CapEED <-
+  "Fig. 1  Typical excitation balance between photosystems in response to wavelength of measuring light. Spectrum was calculated from absorbance spectra of PSII and PSI complexes in solvent (Hogewoning et al. 2012)."
+
+CapETRs <-
+  "Fig. 2  Conceptual diagrams of effects of excitation energy distribution between photosystems (PSII and PSI) on photosynthetic electron transport rate (ETR). Figure shows electron flow (grey lines) and energy flow (black lines) in leaves grown under PSII-light (A, B) and PSI-light (C, D) and measured using PSII-light (A, C) and PSI-light (B, D)."
+
+CapInteract <-
+  "Fig. 3  Conceptual diagram of photosynthetic electron transport rates per absorbed photons by leaves grown under PSII-light (PSII-leaves) and PSI-light (PSI-leaves) in response to the PSII/PSI-bised level of measuring light."
+
+CapSPD <-
+  'Fig. 4  Spectral photon flux density distributions of light provided by (a) fluorescent lamp (FL; FPL55EX-N^1^); metal halide lamp (MH; M220FCELS-W/BUD^1^); high-pressure sodium lamp (HPS; NH220FLS^1^), (b) blue (B; HBL3-3S55-LE^2^), red (R; SRK3-3A80-LE^2^), far-red (FR; L735-36AU^3^), and white (W; NSPW310DS^4^) light-emitting diodes, and (c) incident and leaf-transmitted sunlight. Distributions at a photon flux density of 100 µmol m^--2^ s^--1^ within 400--750 nm are shown. Grey shading indicates far-red waveband (700--750 nm). Distribution of sunlight is from International Electrotechnical Commission Standard 60904–3 [@IEC2007]. Distribution of leaf-transmitted sunlight was calculated from that of incident sunlight and transmittance spectrum of cucumber leaf (Murakami et al. 2016).<br>^1^IWASAKI ELECTRIC CO., LTD, Tokyo, Japan; ^2^Tricon Co., Shimane, Japan; ^3^Epitex Inc., Kyoto, Japan; ^4^Nichia Chemical Industries Ltd., Tokushima, Japan.'
